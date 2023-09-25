c1df7e1a55a3d47ef8935c980b1e4ee8a994f0b7
eudroa00 <109921349+eudroa00@users.noreply.github.com>
2023-09-25 14:26:19 +0300

    closing index feature

--- js/aside.js
+++ js/aside.js
@@ -1,12 +1,22 @@
+
 document.addEventListener('DOMContentLoaded', function () {
   const sidebar = document.getElementById('index');
-  //console.log(sidebar);
   const toggleButton = document.getElementById('toggleSidebarButton');
+  const main = document.getElementById('articles-container');
+  console.log(main);
   const menuLinks = document.querySelectorAll('.menu-link'); // This line of code is not working ;(
 
   function closeSidebar() {
     sidebar.style.left = '-280px';
   }
+  
+  // Temporary fix to the bug in this code ;(
+  main.addEventListener('click', function(){
+    if (sidebar.style.left === '0px' || sidebar.style.left === '') {
+      closeSidebar();
+      toggleButton.textContent = 'Índice';
+    }
+  });
 
   toggleButton.addEventListener('click', function () {
     if (sidebar.style.left === '0px' || sidebar.style.left === '') {
